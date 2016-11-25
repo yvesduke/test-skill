1f1306bde90c3e0e9f7f8be3f3b3a4576204253c
Yves Dukuze <yvesduke06@live.co.uk>
2016-11-25 10:59:40 +0000

    testing mode

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ app.intent('SwitchInputIntent',
   }
 );
  module.exports = app;
- exports.value1 = value1;
+ module.exports = value1;
+ //exports.value1 = value1;
 // Also try to server an html page with alexa's data
 
 
